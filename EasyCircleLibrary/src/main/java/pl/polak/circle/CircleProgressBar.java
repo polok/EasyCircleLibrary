8f4c71d99b068571c77092b2ee7c45e9c7dbbd6b
Marcin Polak <mpolak87@gmail.com>
2014-03-15 11:47:12 +0100

    *added empty sample project

---
+++ EasyCircleLibrary/src/main/java/pl/polak/circle/CircleProgressBar.java
@@ -0,0 +1,24 @@
+package pl.polak.circle;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CircleProgressBar extends View {
+
+    public CircleProgressBar(Context context) {
+        super(context);
+    }
+
+    public CircleProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+
+}
